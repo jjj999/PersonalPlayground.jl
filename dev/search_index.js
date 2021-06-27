8d2129e30b4085f6f0c93142d01072ed5c0b3cbc
Documenter.jl <documenter@juliadocs.github.io>
2021-06-27 09:54:08 +0000

    build based on 6c9b907

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Home","page":"Home","title":"Home","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This useless Julia package is a playground of mine to do experiment somethings about Julia packages.","category":"page"}]
+}
